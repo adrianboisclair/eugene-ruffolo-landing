83d8439a86cf9b2b28d043179468af74c155f69d
Adrian Boisclair <adrianboisclair@gmail.com>
2017-04-13 23:10:26 -0700

    update styles

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,16 +1,29 @@
 var SplashLanding = (function () {
     function SplashLanding() {
         this.video = document.getElementById('video');
+        this.album = document.getElementById('album');
+        this.book = document.getElementById('book');
         this.init();
     }
     SplashLanding.prototype.init = function () {
+        this.playBegin();
+    };
+    SplashLanding.prototype.playBegin = function () {
+        var _this = this;
+        window.setTimeout(function () {
+            _this.video.classList.remove('hideaway');
+            _this.album.classList.remove('hideaway');
+            _this.book.classList.remove('hideaway');
+            console.log(_this.book);
+            _this.video.play();
+        }, 100);
+    };
+    SplashLanding.prototype.playerListener = function () {
         var video = this.video;
-        var listener = 'progress';
-        video.addEventListener(listener, function () {
+        video.addEventListener('progress', function () {
             var loadedPercentage = this.buffered.end(0);
             console.log(loadedPercentage);
         });
-        video.play();
     };
     return SplashLanding;
 })();
--- public/assets/js/script.ts
+++ public/assets/js/script.ts
@@ -1,20 +1,35 @@
-
-
-
 class SplashLanding {
+
     video:any = document.getElementById('video');
+    album:any = document.getElementById('album');
+    book:any = document.getElementById('book');
+
     constructor() {
         this.init();
     }
 
     init() {
+        this.playBegin();
+    }
+
+    playBegin() {
+        let _this = this;
+        window.setTimeout(function(){
+            _this.video.classList.remove('hideaway');
+            _this.album.classList.remove('hideaway');
+            _this.book.classList.remove('hideaway');
+            console.log(_this.book);
+            _this.video.play();
+        }, 100);
+    }
+
+    playerListener() {
         let video = this.video;
-        let listener = 'progress';
-        video.addEventListener(listener, function(){
+
+        video.addEventListener('progress', function(){
             var loadedPercentage = this.buffered.end(0);
             console.log(loadedPercentage);
         });
-        video.play();
     }
 }
 
