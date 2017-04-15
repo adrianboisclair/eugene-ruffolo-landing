fc443c1c2da3fd15ffb14a83062a196c2343db19
Adrian Boisclair <adrianboisclair@gmail.com>
2017-04-15 14:19:11 -0700

    new version with guitar bg

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,6 +6,7 @@ var SplashLanding = (function () {
     function SplashLanding() {
         this.isLoaded = false;
         this.isPlayed = false;
+        this.body = document.querySelector('body');
         this.splash = document.getElementById('landing-splash');
         this.video = document.getElementById('video');
         this.album = document.getElementById('album');
@@ -13,12 +14,10 @@ var SplashLanding = (function () {
         this.logo = document.getElementById('container-logo');
         this.intro = document.getElementById('container-intro');
         this.cta = document.getElementById('container-cta');
-        this.init();
-    }
-    SplashLanding.prototype.init = function () {
         this.playBegin();
         this.ctaClickHandler();
-    };
+        this.videoEndHandler();
+    }
     SplashLanding.prototype.playBegin = function () {
         var _this = this;
         _this.video.play();
@@ -29,42 +28,47 @@ var SplashLanding = (function () {
             _this.logo.classList.remove('hideaway');
             _this.intro.classList.remove('hideaway');
             _this.videoClickHandler();
+            _this.tempSiteSimulator();
             window.setTimeout(function () {
                 _this.cta.classList.remove('hideaway');
-            }, 5000);
+            }, 3000);
         }, 1000);
     };
-    SplashLanding.prototype.playerListener = function () {
-        var video = this.video;
-        var _this = this;
-        video.play();
-        video.addEventListener('progress', function () {
-            var loadedPercentage = this.buffered.end(0);
-            if (loadedPercentage > 0) {
-                _this.isLoaded = true;
-            }
-            if (_this.isLoaded && !_this.isPlayed) {
-                _this.isPlayed = true;
-                _this.playBegin();
-            }
-        });
-        video.addEventListener('ended', _this.videoEndHandler, false);
-    };
+    //playerListener() {
+    //    let video = this.video;
+    //    let _this = this;
+    //
+    //    video.play();
+    //
+    //    video.addEventListener('progress', function(){
+    //        var loadedPercentage = this.buffered.end(0);
+    //        if(loadedPercentage > 0) {
+    //            _this.isLoaded = true;
+    //        }
+    //        if(_this.isLoaded && !_this.isPlayed) {
+    //            _this.isPlayed = true;
+    //            _this.playBegin();
+    //        }
+    //    });
+    //
+    //    video.addEventListener('ended', _this.videoEndHandler, false);
+    //}
     SplashLanding.prototype.videoClickHandler = function () {
-        var _this = this;
-        return this.video.addEventListener('click', function (e) {
-            if (_this.video.paused) {
-                _this.video.play();
+        var video = this.video;
+        return video.addEventListener('click', function (e) {
+            if (video.paused) {
+                video.play();
             }
             else {
-                _this.video.pause();
+                video.pause();
             }
         });
     };
     SplashLanding.prototype.ctaClickHandler = function () {
         var _this = this;
-        return this.cta.addEventListener('click', function (e) {
-            _this.splash.classList.add('fadeOut');
+        var splash = this.splash;
+        return _this.cta.addEventListener('click', function (e) {
+            splash.classList.add('fadeOut');
             _this.volumeFadeOut();
             window.setTimeout(function () {
                 _this.splash.remove();
@@ -72,22 +76,25 @@ var SplashLanding = (function () {
         });
     };
     SplashLanding.prototype.volumeFadeOut = function () {
-        console.log('volumeFadeOut');
         var _this = this;
         var handle = setInterval(myFunc, 100);
         function myFunc() {
-            var currentVol = _this.video.volume;
             try {
-                _this.video.volume = currentVol - .02;
+                _this.video.volume = _this.video.volume - .02;
             }
             catch (e) {
-                console.log('stopping video...');
                 clearTimeout(handle);
             }
         }
     };
     SplashLanding.prototype.videoEndHandler = function () {
-        return this.cta.click();
+        var _this = this;
+        return _this.video.addEventListener('ended', function (e) {
+            _this.cta.click();
+        }, false);
+    };
+    SplashLanding.prototype.tempSiteSimulator = function () {
+        return this.body.classList.add('activated');
     };
     return SplashLanding;
 })();
--- public/assets/js/script.ts
+++ public/assets/js/script.ts
@@ -6,6 +6,7 @@ class SplashLanding {
 
     isLoaded:boolean = false;
     isPlayed:boolean = false;
+    body:any = document.querySelector('body');
     splash:any = document.getElementById('landing-splash');
     video:any = document.getElementById('video');
     album:any = document.getElementById('album');
@@ -15,12 +16,9 @@ class SplashLanding {
     cta:any = document.getElementById('container-cta');
 
     constructor() {
-        this.init();
-    }
-
-    init() {
         this.playBegin();
         this.ctaClickHandler();
+        this.videoEndHandler();
     }
 
     playBegin() {
@@ -33,47 +31,49 @@ class SplashLanding {
             _this.logo.classList.remove('hideaway');
             _this.intro.classList.remove('hideaway');
             _this.videoClickHandler();
+            _this.tempSiteSimulator();
             window.setTimeout(function(){
                 _this.cta.classList.remove('hideaway');
-            }, 5000);
+            }, 3000);
         }, 1000);
     }
 
-    playerListener() {
-        let video = this.video;
-        let _this = this;
-
-        video.play();
-
-        video.addEventListener('progress', function(){
-            var loadedPercentage = this.buffered.end(0);
-            if(loadedPercentage > 0) {
-                _this.isLoaded = true;
-            }
-            if(_this.isLoaded && !_this.isPlayed) {
-                _this.isPlayed = true;
-                _this.playBegin();
-            }
-        });
-
-        video.addEventListener('ended', _this.videoEndHandler, false);
-    }
+    //playerListener() {
+    //    let video = this.video;
+    //    let _this = this;
+    //
+    //    video.play();
+    //
+    //    video.addEventListener('progress', function(){
+    //        var loadedPercentage = this.buffered.end(0);
+    //        if(loadedPercentage > 0) {
+    //            _this.isLoaded = true;
+    //        }
+    //        if(_this.isLoaded && !_this.isPlayed) {
+    //            _this.isPlayed = true;
+    //            _this.playBegin();
+    //        }
+    //    });
+    //
+    //    video.addEventListener('ended', _this.videoEndHandler, false);
+    //}
 
     videoClickHandler() {
-        let _this = this;
-        return this.video.addEventListener('click', function(e) {
-            if(_this.video.paused) {
-                _this.video.play();
+        let video = this.video;
+        return video.addEventListener('click', function(e) {
+            if(video.paused) {
+                video.play();
             }else{
-                _this.video.pause();
+                video.pause();
             }
         });
     }
 
     ctaClickHandler() {
         let _this = this;
-        return this.cta.addEventListener('click', function(e){
-            _this.splash.classList.add('fadeOut');
+        let splash = this.splash;
+        return _this.cta.addEventListener('click', function(e){
+            splash.classList.add('fadeOut');
             _this.volumeFadeOut();
             window.setTimeout(function() {
                 _this.splash.remove();
@@ -82,23 +82,28 @@ class SplashLanding {
     }
 
     volumeFadeOut() {
-        console.log('volumeFadeOut');
-        var _this = this;
-        var handle = setInterval(myFunc,100);
+        let _this = this;
+        let handle = setInterval(myFunc,100);
         function myFunc() {
-            var currentVol = _this.video.volume;
             try{
-                _this.video.volume = currentVol - .02;
+                _this.video.volume = _this.video.volume - .02;
             } catch (e) {
-                console.log('stopping video...');
                 clearTimeout(handle);
             }
         }
     }
 
     videoEndHandler() {
-        return this.cta.click();
+        let _this = this;
+        return _this.video.addEventListener('ended', function(e){
+            _this.cta.click();
+        }, false);
     }
+
+    tempSiteSimulator() {
+        return this.body.classList.add('activated');
+    }
+
 }
 
 var splashLanding = new SplashLanding();
